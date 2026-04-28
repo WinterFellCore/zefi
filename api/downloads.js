91615c4f51c9a9915603d97c19af50aa69d4913c
WinterFellCore <bobruapela@gmail.com>
2026-04-27 23:59:11 -0300

    Fix avatar border-radius and remove old downloads

--- api/downloads.js
+++ api/downloads.js
@@ -5,19 +5,19 @@ const DOWNLOADS = [
     id: 'ff-max-x86',
     name: 'Free Fire MAX X86',
     description: 'Free Fire MAX para dispositivos x86 - OB53. Compatível com emuladores e dispositivos Intel.',
-    url: 'https://www.mediafire.com/file/669xh6lc1rjfsl8/Free_Fire_MAX_X86_OB53_%25281%2529.xapk/file',
+    url: 'https://download2290.mediafire.com/0oxxyxof0bsgHNjxCoLEKtrWnFnAbRJuNgapwSQ9r1ziI-LioTdRRCVY22TmkDXtjJ5p6SU36wg-tILdjBAkySKZG60i8d59j6hylph5MQYYCpQS1dHjMQpPsj3AAEDuDgIyfsnqzybraLdwzgBP5BC0ryqzKlGD7__nD2SDyHhavis/669xh6lc1rjfsl8/Free+Fire+MAX+X86+OB53+%281%29.xapk',
     image: 'https://is1-ssl.mzstatic.com/image/thumb/PurpleSource221/v4/41/b5/1c/41b51c10-c3df-b309-de20-f3aa04b9d0d5/Placeholder.mill/400x400bb.webp',
     version: 'OB53',
-    size: '~800 MB',
+    size: '~440 MB',
   },
   {
     id: 'ff-v7a',
     name: 'Free Fire V7A',
     description: 'Free Fire para dispositivos ARM64-v8a - OB53. Versão otimizada para smartphones Android.',
     url: 'https://download2288.mediafire.com/gq9kewctgczgLLk8_A5LSRll_HzggZEd0zX3XiQNt-S98BCs6qHEoIp1Rq65LQJf350QFCIYFh9I1g9SAh3ajFuCbMUYHwspzcaOI3fDkq2ASymWVD0bcyPbX8er9TutuuB2t8ZS4fEcRROi2mfahO8Y_Oivw5vUlgch-aJkXPY0rOY/xpqrafanwfhwsgt/Free+fire+V7A+OB53+%281%29.xapk',
-    image: 'https://is1-ssl.mzstatic.com/image/thumb/PurpleSource221/v4/41/b5/1c/41b51c10-c3df-b309-de20-f3aa04b9d0d5/Placeholder.mill/400x400bb.webp',
+    image: 'https://is1-ssl.mzstatic.com/image/thumb/PurpleSource211/v4/3c/04/6e/3c046e77-21e0-4db0-0289-c283644dfcf7/Placeholder.mill/400x400bb.webp',
     version: 'OB53',
-    size: '~700 MB',
+    size: '~350 MB',
   },
 ];
 
