6378fb7e90716ddb99c2467b4f6d22b7fe4809ce
WinterFellCore <bobruapela@gmail.com>
2026-05-02 11:51:19 -0300

    feat: adicionar ícones na sidebar (Início, Produtos, Downloads, Discord) e deixar só Login no header

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,21 +1,5 @@
 export const nav = [
 	{
-		title: 'Início',
-		slug: '/',
-	},
-	{
-		title: 'Produtos',
-		slug: '/#produtos',
-	},
-	{
-		title: 'Downloads',
-		slug: '/downloads',
-	},
-	{
-		title: 'Discord',
-		slug: 'https://discord.gg/jsymg94sXB',
-	},
-	{
 		title: 'Login',
 		slug: '/login',
 	},
